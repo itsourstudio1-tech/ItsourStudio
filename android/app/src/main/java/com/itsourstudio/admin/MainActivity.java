9492b11b62820980881a0704f7f3b927a2d0fecb
Joseph Venedict Tillo <119979348+Sedictt@users.noreply.github.com>
2025-12-19 14:11:04 +0800

    apk

---
+++ android/app/src/main/java/com/itsourstudio/admin/MainActivity.java
@@ -0,0 +1,5 @@
+package com.itsourstudio.admin;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.itsourstudio.admin',
+  appName: 'ItsOurStudioAdmin',
+  webDir: 'dist'
+};
+
+export default config;
